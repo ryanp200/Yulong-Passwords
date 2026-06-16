12341a15ed9ed6edae63894c6a43257e61e5196e
Ryan <ryan.peres2311@gmail.com>
2026-06-16 09:54:15 -0300

    correções

--- paginas/components/deslogar.php
+++ paginas/components/deslogar.php
@@ -1,6 +1,7 @@
 <?php
+    session_start();
     if(isset($_SESSION['usuario'])){
-        $_SESSION['usuario'] = "";
+        unset($_SESSION['usuario']); 
         header("Location: ../../index.php");
     }else{
         header("Location: ../../index.php");
